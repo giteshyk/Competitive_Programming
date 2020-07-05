edd6906c9822d2d96eca2b5a10ebf5bbef4f6a27
Gitesh Katre <61425961+giteshyk@users.noreply.github.com>
2020-07-05 20:02:16 +0530

    Add files via upload
    Input Format. The first line contains an integer 𝑑. The second line contains an integer 𝑚. The third line
    specifies an integer 𝑛. Finally, the last line contains integers stop1
    ,stop2
    , . . . ,stop𝑛.
    
    Output Format. Assuming that the distance between the cities is 𝑑 miles, a car can travel at most 𝑚 miles
    on a full tank, and there are gas stations at distances stop1
    ,stop2
    , . . . ,stop𝑛 along the way, output the
    minimum number of refills needed. Assume that the car starts with a full tank. If it is not possible to
    reach the destination, output −1.
    
    Constraints. 1 ≤ 𝑑 ≤ 105
    . 1 ≤ 𝑚 ≤ 400. 1 ≤ 𝑛 ≤ 300. 0 < stop1 < stop2 < · · · < stop𝑛 < 𝑑.
    S

---
+++ car_fueling.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::vector;
+using std::max;
+
+int compute_min_refills(int d, int m, vector<int> & stops,int n) {
+    int curr_max = m, curr_stop ;
+    int refills = 0;
+    int max_refill = 0;
+
+    if(m>d)
+        return 0;
+
+    while(curr_max <= d && max_refill <=n+1 ){
+        int i = 0;
+
+        while(curr_max - stops[i] >= 0 && i<n)
+            i++;
+
+        curr_stop = stops[i-1] ;
+        if(curr_max < d && curr_stop < d)
+            refills += 1;
+        curr_max = curr_stop + m ;
+        max_refill += 1 ;
+
+        if(curr_max >= d)
+            return refills ;
+    }
+return -1;
+}
+
+
+int main() {
+    int d = 0;
+    cin >> d;
+    int m = 0;
+    cin >> m;
+    int n = 0;
+    cin >> n;
+
+    vector<int> stops(n);
+    for (size_t i = 0; i < n; ++i)
+        cin >> stops.at(i);
+
+    cout << compute_min_refills(d, m, stops ,n) << "\n";
+
+    return 0;
+}
