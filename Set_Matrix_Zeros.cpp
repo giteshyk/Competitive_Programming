299189755a89451e193644d8264d3829e46df8b2
Gitesh Katre <61425961+giteshyk@users.noreply.github.com>
2020-07-14 22:40:31 +0530

    Create Set_Matrix_Zeros.cpp
    Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in-place.

---
+++ Set_Matrix_Zeros.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        bool isCol = false;
+        
+        for(int i=0;i < rows;i++){
+            if(matrix[i][0]==0)
+                isCol = true;
+        
+        for(int j=1;j<cols;j++){
+            if(matrix[i][j]==0){
+                matrix[0][j] = 0;
+                matrix[i][0] = 0;
+            }
+        }
+      
+    } //first for
+     
+        for(int i =1; i<rows;i++){
+            for(int j=1;j <cols;j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }  
+        if(matrix[0][0]==0){
+            for(int j=0;j<cols;j++){
+                matrix[0][j] = 0;
+            }
+        }
+        
+        if(isCol){
+            for(int i=0;i < rows;i++){
+                matrix[i][0] = 0;
+            }
+        }       
+    }      
+};
