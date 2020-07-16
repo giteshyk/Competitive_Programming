fd04b4a9e2ef9ff5d0a783162cc1e4eb179a116c
Gitesh Katre <61425961+giteshyk@users.noreply.github.com>
2020-07-16 22:03:47 +0530

    Update unique_paths.cpp

--- unique_paths.cpp
+++ unique_paths.cpp
@@ -1,30 +1,25 @@
 class Solution {
   public:
     int uniquePaths(int m,int n) {
-        
         long result;
         int j = 1;
         
         if(m==1 || n==1)
-            return 1;
-            
+            return 1; 
+      
         m--;
-        n--;
-        
+        n--; 
+      
         if(m<n){
             m = m+n;
             n = m-n;
             m = m-n;
-        }
-        
-        
+        }       
         for(int i=m+1; i <= m+n; i++,j++){
             result *= i;
             result /= j;
         }
     
-    return (int)result;
-    
-    }
-    
+    return (int)result;  
+    }  
 };
