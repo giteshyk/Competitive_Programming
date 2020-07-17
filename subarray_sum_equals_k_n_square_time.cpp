035ae371c8e046bf91b9760466f0305b98bb3d1f
Gitesh Katre <61425961+giteshyk@users.noreply.github.com>
2020-07-17 22:38:39 +0530

    Create subarray_sum_equals_k_n_square_time.cpp
    Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
    
    Example 1:
    
    Input:nums = [1,1,1], k = 2
    Output: 2
     
    
    Constraints:
    
    The length of the array is in range [1, 20,000].
    The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].

---
+++ subarray_sum_equals_k_n_square_time.cpp
@@ -0,0 +1,20 @@
+//NAIVE approach to the problem take O(n^2) time and O(1) space complexity .
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int count = 0;
+        
+        for(int i=0;i<nums.size();i++){
+          int sum = 0;
+          for(int j=i;j<nums.size();j++){
+            sum += nums[j];
+            if(sum==k){
+              count++;
+            }
+          }
+        }
+        return count;
+    }
+};
+
