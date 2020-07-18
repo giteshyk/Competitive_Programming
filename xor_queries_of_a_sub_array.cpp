7e4aa010b9df29b2a47a7674a9ea851e5ee38843
Gitesh Katre <61425961+giteshyk@users.noreply.github.com>
2020-07-18 15:04:53 +0530

    Update xor_queries_of_a_sub_array.cpp

--- xor_queries_of_a_sub_array.cpp
+++ xor_queries_of_a_sub_array.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+  public:
+    vector<int> xorQueries(vector<int>& arr,vecotr<int>& queries){
+        vector<int> prefixXOR(arr.size()+1,0);
+        vector<int> result;
+        
+        for(int i=0;i < arr.size();i++){
+            prefixXOR[i+1] = prefixXOR[i] ^ arr[i];
+        }
+        
+        for(vector<int>& temp:queries){
+            result.push_back(prefixXOR[temp[0]]^prefixXOR[temp[1]+1]);
+        }
+    return result;
+    }
+};
