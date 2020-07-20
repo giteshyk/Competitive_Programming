00fd90108bd3873ca000886228576b69b5ce1c5c
Gitesh Katre <61425961+giteshyk@users.noreply.github.com>
2020-07-20 14:59:22 +0530

    Update intersection_of_two_linked_list.cpp

--- intersection_of_two_linked_list.cpp
+++ intersection_of_two_linked_list.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+  public:
+    node* intersection_of_Y_linked_list(node* headA,node* headB) {
+	    node* ptrA = headA;
+	    node* ptrB = headB;
+	    
+	    while(ptrA != ptr B){
+	    	ptrA = ptrA ? ptrA -> next : headB ;
+		ptrB = ptrB ? ptrB -> next : headA ;
+	    }
+     return ptrA ;
+    }
+};
