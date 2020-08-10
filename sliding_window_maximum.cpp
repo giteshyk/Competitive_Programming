741bd1a607c69ac5a6e30d7577e91a2b1c23bdf8
Gitesh Katre <61425961+giteshyk@users.noreply.github.com>
2020-08-10 20:35:32 +0530

    Create sliding_window_maximum.cpp
    Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position. Return the max sliding window.
    
    Follow up:
    Could you solve it in linear time?
    
    Example:
    
    Input: nums = [1,3,-1,-3,5,3,6,7], and k = 3
    Output: [3,3,5,5,6,7] 
    Explanation: 
    
    Window position                Max
    ---------------               -----
    [1  3  -1] -3  5  3  6  7       3
     1 [3  -1  -3] 5  3  6  7       3
     1  3 [-1  -3  5] 3  6  7       5
     1  3  -1 [-3  5  3] 6  7       5
     1  3  -1  -3 [5  3  6] 7       6
     1  3  -1  -3  5 [3  6  7]      7

---
+++ sliding_window_maximum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {         
+        vector<int> res;
+        deque<int> buffer;
+        
+        for(auto i = 0; i < nums.size(); i++){
+            while(!buffer.empty() && nums[i] >= nums[buffer.back()])
+                buffer.pop_back();
+            buffer.push_back(i);
+            
+            if(i >= k-1)
+                res.push_back(nums[buffer.front()]);
+            
+            if(buffer.front() <= i-k+1)
+                buffer.pop_front();
+        }
+        return res;
+    }
+};
