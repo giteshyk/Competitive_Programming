68863996eb5dd5355b2514a791289bb44f24aa81
Gitesh Katre <61425961+giteshyk@users.noreply.github.com>
2020-08-14 19:29:55 +0530

    Create post_order.cpp
    Given a binary tree, return the postorder traversal of its nodes' values.
    
    Example:
    Input: [1,null,2,3]
       1
        \
         2
        /
       3
    
    Output: [3,2,1]
    Follow up: Recursive solution is trivial, could you do it iteratively?

---
+++ post_order.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> nodes;
+        postorder(root, nodes);
+        return nodes;
+    }
+    
+    void postorder(TreeNode* root, vector<int>& nodes) {
+        if (!root) {
+            return;
+        }
+        postorder(root -> left, nodes);
+        postorder(root -> right, nodes);
+        nodes.push_back(root -> val);
+    }
+    
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> nodes;
+        stack<TreeNode*> todo;
+        TreeNode* last = NULL;
+        while (root || !todo.empty()) {
+            if (root) {
+                todo.push(root);
+                root = root -> left;
+            } else {
+                TreeNode* node = todo.top();
+                if (node -> right && last != node -> right) {
+                    root = node -> right;
+                } else {
+                    nodes.push_back(node -> val);
+                    last = node;
+                    todo.pop();
+                }
+            }
+        }
+        return nodes;
+    }
+};
